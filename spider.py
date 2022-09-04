7a0895632b7a17622875c9b142bdc86745ba572e
guolin <guolin1633@163.com>
2022-09-04 12:13:20 +0800

    根root正则表达式分析

--- spider.py
+++ spider.py
@@ -1,8 +1,12 @@
+import re
 # 导入获取html的库
 from urllib import request
 
 class Spider():
     url = 'https://www.huya.com/g/lol'
+    # 根正则表达式
+    root_pattern = '<li class="game-live-item" data-gid=[\s\S]*?</li>'
+    
 
     # 私有方法，获取网页的内容
     def __fetch_content(self):
@@ -11,13 +15,14 @@ class Spider():
         htmls = r.read()
         # 将字节码转换为字符串
         htmls = str(htmls, encoding='utf-8')
-        # 断点
-        a = 1
         return htmls
 
     # 分析函数
     def __analysis(self, htmls):
-        pass
+        root_html = re.findall(Spider.root_pattern, htmls)
+        print(root_html[0])
+        # 断点
+        a = 1
 
     # Spider的入口方法
     def go(self):
