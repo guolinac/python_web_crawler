4b9e1a025de02b60b78d32ec437b6feae95600d5
guolin <guolin1633@163.com>
2022-09-04 11:37:50 +0800

    编码转换

--- spider.py
+++ spider.py
@@ -7,11 +7,22 @@ class Spider():
     # 私有方法，获取网页的内容
     def __fetch_content(self):
         r = request.urlopen(Spider.url)
+        # bytes
         htmls = r.read()
+        # 将字节码转换为字符串
+        htmls = str(htmls, encoding='utf-8')
+        # 断点
+        a = 1
+        return htmls
+
+    # 分析函数
+    def __analysis(self, htmls):
+        pass
 
     # Spider的入口方法
     def go(self):
-        self.__fetch_content()
+        htmls = self.__fetch_content()
+        self.__analysis(htmls)
 
 spider = Spider()
 spider.go()
